4dab934be88112e5a16ce457e1a0185304b2b549
supun456 <pakaya*351>
2025-07-01 21:48:50 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"/chroma-tech-space",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
